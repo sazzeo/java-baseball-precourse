7a935d636997f371eeed1bdb7c51426a3f05d8be
jiyeong-dt <imjee02@gmail.com>
2023-07-04 20:22:53 +0900

    remove : init으로 돌아감

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,8 +6,5 @@ public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
 
-
-        //숫자를 입력해주세요.
-        String userNumber = Console.readLine();
     }
 }
