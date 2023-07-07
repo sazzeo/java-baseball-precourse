9743ba1bfb2d29ba6a3dcd7eaa6076de0e3f3d15
jy lim <imjee02@gmail.com>
2023-07-07 10:32:50 +0900

    remove: 프로젝트 초기화, 세번째 시도

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,8 @@
 package baseball;
 
-import baseball.domain.BaseballNumber;
 
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
-        BaseballNumber baseballNumber = new BaseballNumber();
     }
 }
