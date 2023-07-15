da88f75fa6ea7fd6ad768edffb0ccc5c89d224b5
jiyeong-dt <imjee02@gmail.com>
2023-07-15 23:31:03 +0900

    feat: Number 생성 - 수는 1~9까지의 숫자여야한다.

---
+++ src/main/java/baseball/domain/BaseballSetting.java
@@ -0,0 +1,8 @@
+package baseball.domain;
+
+public class BaseballSetting {
+    public static final int DIGITS = 3;
+    public static final int MIN_NUM = 1;
+    public static final int MAX_NUM = 9;
+
+}
---
+++ src/main/java/baseball/domain/Number.java
@@ -0,0 +1,33 @@
+package baseball.domain;
+
+
+import java.util.Objects;
+
+public class Number {
+
+    private int number;
+
+    public Number(final int number) {
+        this.validateRange(number);
+        this.number = number;
+    }
+
+    private void validateRange(final int number) {
+        if (number < BaseballSetting.MIN_NUM || number > BaseballSetting.MAX_NUM) {
+            throw new IllegalArgumentException("숫자의 범위는 " + BaseballSetting.MIN_NUM + "~" + BaseballSetting.MAX_NUM + " 사이여야 합니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Number)) return false;
+        final Number number1 = (Number) o;
+        return number == number1.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+}
