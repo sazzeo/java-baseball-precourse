354baa8de04a6d06833138eb44b5d23b95dfd3ab
jiyeong-dt <imjee02@gmail.com>
2023-07-15 22:36:11 +0900

    remove : base-template 분리

--- src/main/java/baseball/config/BaseballSetting.java
+++
@@ -1,7 +0,0 @@
-package baseball.config;
-
-public class BaseballSetting {
-    public static final int DIGITS = 3;
-    public static final int MIN_NUMBER = 1;
-    public static final int MAX_NUMBER = 9;
-}
